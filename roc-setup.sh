24318085b7c58d2e11cd3a7b5e2f3ed2afd512a6
Kent Knox <kent.knox@amd>
2016-04-22 18:18:42 -0500

    Refactoring after ROCm 1.0 release
    ROCm installation paths have changed, and the 1.0 release created a
    new installation heirarchy.

--- roc-setup.sh
+++ roc-setup.sh
@@ -68,16 +68,16 @@ done
 # hcc-hsail does not have a develop branch
 export repo_branch_hcc_hsail="master"
 
-# hcc-isa conforms to a non git-flow naming scheme, 'master' changes the most
-export repo_branch_hcc_isa=
+# hcc-lc conforms to a non git-flow naming scheme, 'master' changes the most
+export repo_branch_hcc_lc=
 
 export repo_branch=
 if [ -n "${build_master}" ]; then
   repo_branch="master"
-  repo_branch_hcc_isa="testing"
+  repo_branch_hcc_lc="testing"
 else
   repo_branch="dev"
-  repo_branch_hcc_isa="master"
+  repo_branch_hcc_lc="master"
 fi
 
 export build_config=
@@ -85,12 +85,12 @@ export build_config_roct=
 export roct_cleanup=
 export rocr_cleanup=
 export hcc_hsail_cleanup=
-export hcc_isa_cleanup=
+export hcc_lc_cleanup=
 export rock_name=
 export roct_name=
 export rocr_name=
 export hcc_hsail_name=
-export hcc_isa_name=
+export hcc_lc_name=
 
 rocm_prefix="rocm/"
 if [ -n "${build_release}" ]; then
@@ -100,7 +100,7 @@ if [ -n "${build_release}" ]; then
   roct_name="${rocm_prefix}roct-${repo_branch}"
   rocr_name="${rocm_prefix}rocr-${repo_branch}"
   hcc_hsail_name="${rocm_prefix}hcc-hsail-${repo_branch_hcc_hsail}"
-  hcc_isa_name="${rocm_prefix}hcc-isa-${repo_branch_hcc_isa}"
+  hcc_lc_name="${rocm_prefix}hcc-lc-${repo_branch_hcc_lc}"
 
   # Custom commands to clean up build directories for each component
   # This is to keep release images as small as possible
@@ -108,7 +108,7 @@ if [ -n "${build_release}" ]; then
   roct_cleanup='cd ~ && rm -rf ${HSATHK_BUILD_PATH} &&'
   rocr_cleanup='cd ~ && rm -rf ${ROCR_BUILD_PATH} &&'
   hcc_hsail_cleanup='cd ~ && rm -rf ${HCC_BUILD_PATH} &&'
-  hcc_isa_cleanup='cd ~ && rm -rf ${HCC_BUILD_PATH} &&'
+  hcc_lc_cleanup='cd ~ && rm -rf ${HCC_BUILD_PATH} &&'
 else
   build_config='Debug'
 
@@ -118,18 +118,20 @@ else
   roct_name="${rocm_prefix}roct-${repo_branch}-${build_config,}"
   rocr_name="${rocm_prefix}rocr-${repo_branch}-${build_config,}"
   hcc_hsail_name="${rocm_prefix}hcc-hsail-${repo_branch_hcc_hsail}-${build_config,}"
-  hcc_isa_name="${rocm_prefix}hcc-isa-${repo_branch_hcc_isa}-${build_config,}"
+  hcc_lc_name="${rocm_prefix}hcc-lc-${repo_branch_hcc_lc}-${build_config,}"
 fi
 
+export rock_volume='/opt/rocm/rock/'
 export roct_volume='/opt/rocm/libhsakmt/'
 export rocr_volume='/opt/rocm/hsa/'
-export hcc_volume='/opt/rocm/hcc/'
+export hcc_hsail_volume='/opt/rocm/hcc-hsail/'
+export hcc_lc_volume='/opt/rocm/hcc-lc/'
 
 # Uncomment below to print dockerfiles with template substitutions; debugging
-cat rock/rock-deb-dockerfile.template | envsubst '${repo_branch}' > rock/Dockerfile
+cat rock/rock-deb-dockerfile.template | envsubst '${repo_branch}:${rock_volume}' > rock/Dockerfile
 cat roct/roct-thunk-dockerfile.template | envsubst '${rock_name}:${repo_branch}:${build_config_roct}:${roct_cleanup}:${roct_volume}' > roct/Dockerfile
-cat rocr/rocr-make-dockerfile.template | envsubst '${roct_name}:${repo_branch}:${build_config}:${rocr_cleanup}:${rocr_volume}' > rocr/Dockerfile
-cat hcc-hsail/hcc-hsail-dockerfile.template | envsubst '${rocr_name}:${repo_branch_hcc_hsail}:${build_config}:${hcc_hsail_cleanup}:${hcc_volume}' > hcc-hsail/Dockerfile
-cat hcc-isa/hcc-isa-dockerfile.template | envsubst '${rocr_name}:${repo_branch_hcc_isa}:${build_config}:${hcc_isa_cleanup}:${hcc_volume}' > hcc-isa/Dockerfile
+cat rocr/rocr-make-dockerfile.template | envsubst '${roct_name}:${repo_branch}:${build_config}:${rocr_cleanup}:${roct_volume}:${rocr_volume}' > rocr/Dockerfile
+cat hcc-hsail/hcc-hsail-dockerfile.template | envsubst '${rocr_name}:${repo_branch_hcc_hsail}:${build_config}:${hcc_hsail_cleanup}:${roct_volume}:${rocr_volume}:${hcc_hsail_volume}' > hcc-hsail/Dockerfile
+cat hcc-lc/hcc-lc-dockerfile.template | envsubst '${rocr_name}:${repo_branch_hcc_lc}:${build_config}:${hcc_lc_cleanup}:${roct_volume}:${rocr_volume}:${hcc_lc_volume}' > hcc-lc/Dockerfile
 
-cat docker-compose.yml.template | envsubst '${hcc_isa_name}:${hcc_hsail_name}:${rocr_name}:${roct_name}:${hcc_volume}:${rocr_volume}:${roct_volume}' > docker-compose.yml
+cat docker-compose.yml.template | envsubst '${hcc_lc_name}:${hcc_hsail_name}:${rocr_name}:${roct_name}:${hcc_hsail_volume}:${hcc_lc_volume}:${rocr_volume}:${roct_volume}' > docker-compose.yml
