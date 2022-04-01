8869d75a91383fc56ee240819d0045ce43092aa9
sunway513 <peng.sun@amd.com>
2022-04-01 01:32:44 +0000

    update for rocm5.1

--- build_all.sh
+++ build_all.sh
@@ -1,36 +1,29 @@
-ROCM_VERSION=4.5.2
-AMDGPU_VERSION=21.40.2
+ROCM_VERSION=5.1
+AMDGPU_VERSION=22.10
 cp -r scripts rocm-terminal
 cp -r scripts dev
 
 # build rocm-terminal
 cd rocm-terminal/
-sudo docker build . -f Dockerfile -t rocm/rocm-terminal:$ROCM_VERSION --no-cache --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
-sudo docker build . -f Dockerfile.post-install --build-arg=base_image=rocm/rocm-terminal:$ROCM_VERSION -t rocm/rocm-terminal:$ROCM_VERSION
+sudo docker build . -f Dockerfile -t rocm/rocm-terminal:$ROCM_VERSION --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
 sudo docker tag rocm/rocm-terminal:$ROCM_VERSION rocm/rocm-terminal:latest
 
 #build dev dockers
 cd ../dev
 #centos-7
-sudo docker build . -f Dockerfile-centos-7 -t rocm/dev-centos-7:$ROCM_VERSION --no-cache --build-arg=ROCM_VERSION=$ROCM_VERSION  --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
-sudo docker build . -f Dockerfile.post-install-centos --build-arg=base_image=rocm/dev-centos-7:$ROCM_VERSION -t rocm/dev-centos-7:$ROCM_VERSION
+sudo docker build . -f Dockerfile-centos-7 -t rocm/dev-centos-7:$ROCM_VERSION --build-arg=ROCM_VERSION=$ROCM_VERSION  --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
 sudo docker tag rocm/dev-centos-7:$ROCM_VERSION rocm/dev-centos-7:latest
 
 #ubuntu20.04
-sudo docker build . -f Dockerfile-ubuntu-20.04 -t rocm/dev-ubuntu-20.04:$ROCM_VERSION --no-cache --build-arg=ROCM_VERSION=$ROCM_VERSION  --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
-sudo docker build . -f Dockerfile.post-install --build-arg=base_image=rocm/dev-ubuntu-20.04:$ROCM_VERSION -t rocm/dev-ubuntu-20.04:$ROCM_VERSION
+sudo docker build . -f Dockerfile-ubuntu-20.04 -t rocm/dev-ubuntu-20.04:$ROCM_VERSION --build-arg=ROCM_VERSION=$ROCM_VERSION  --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
 sudo docker tag rocm/dev-ubuntu-20.04:$ROCM_VERSION rocm/dev-ubuntu-20.04:latest
 
 #ubuntu18.04
-sudo docker build . -f Dockerfile-ubuntu-18.04 -t rocm/dev-ubuntu-18.04:$ROCM_VERSION --no-cache --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
-sudo docker build . -f Dockerfile.post-install --build-arg=base_image=rocm/dev-ubuntu-18.04:$ROCM_VERSION -t rocm/dev-ubuntu-18.04:$ROCM_VERSION
+sudo docker build . -f Dockerfile-ubuntu-18.04 -t rocm/dev-ubuntu-18.04:$ROCM_VERSION  --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
 sudo docker tag rocm/dev-ubuntu-18.04:$ROCM_VERSION rocm/dev-ubuntu-18.04:latest
 
 #ubuntu18.04 complete
-sudo docker build . -f Dockerfile-ubuntu-18.04-complete -t rocm/dev-ubuntu-18.04:$ROCM_VERSION-complete --no-cache --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
-sudo docker build . -f Dockerfile.post-install --build-arg=base_image=rocm/dev-ubuntu-18.04:$ROCM_VERSION-complete -t rocm/dev-ubuntu-18.04:$ROCM_VERSION-complete
+sudo docker build . -f Dockerfile-ubuntu-18.04-complete -t rocm/dev-ubuntu-18.04:$ROCM_VERSION-complete --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
 
 #ubuntu20.04 complete
-sudo docker build . -f Dockerfile-ubuntu-20.04-complete -t rocm/dev-ubuntu-20.04:$ROCM_VERSION-complete --no-cache --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
-sudo docker build . -f Dockerfile.post-install --build-arg=base_image=rocm/dev-ubuntu-20.04:$ROCM_VERSION-complete -t rocm/dev-ubuntu-20.04:$ROCM_VERSION-complete
-
+sudo docker build . -f Dockerfile-ubuntu-20.04-complete -t rocm/dev-ubuntu-20.04:$ROCM_VERSION-complete --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
