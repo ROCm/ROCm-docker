20a178cd0d79fc7d4c8b90d12f3bb78e7b24a499
sunway513 <peng.sun@amd.com>
2022-10-02 01:16:38 +0000

    Add ubuntu22.04 base dockers

--- build_all.sh
+++ build_all.sh
@@ -18,12 +18,12 @@ sudo docker tag rocm/dev-centos-7:$ROCM_VERSION rocm/dev-centos-7:latest
 sudo docker build . -f Dockerfile-ubuntu-20.04 -t rocm/dev-ubuntu-20.04:$ROCM_VERSION --build-arg=ROCM_VERSION=$ROCM_VERSION  --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
 sudo docker tag rocm/dev-ubuntu-20.04:$ROCM_VERSION rocm/dev-ubuntu-20.04:latest
 
-#ubuntu18.04
-sudo docker build . -f Dockerfile-ubuntu-18.04 -t rocm/dev-ubuntu-18.04:$ROCM_VERSION  --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
-sudo docker tag rocm/dev-ubuntu-18.04:$ROCM_VERSION rocm/dev-ubuntu-18.04:latest
+#ubuntu22.04
+sudo docker build . -f Dockerfile-ubuntu-22.04 -t rocm/dev-ubuntu-22.04:$ROCM_VERSION  --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
+sudo docker tag rocm/dev-ubuntu-22.04:$ROCM_VERSION rocm/dev-ubuntu-22.04:latest
 
-#ubuntu18.04 complete
-sudo docker build . -f Dockerfile-ubuntu-18.04-complete -t rocm/dev-ubuntu-18.04:$ROCM_VERSION-complete --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
+#ubuntu22.04 complete
+sudo docker build . -f Dockerfile-ubuntu-22.04-complete -t rocm/dev-ubuntu-22.04:$ROCM_VERSION-complete --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
 
 #ubuntu20.04 complete
 sudo docker build . -f Dockerfile-ubuntu-20.04-complete -t rocm/dev-ubuntu-20.04:$ROCM_VERSION-complete --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
