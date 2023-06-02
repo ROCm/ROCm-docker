2dc3478ba7c45cb5033b656afaea613f107463f3
Jeff Daily <jeff.daily@amd.com>
2023-06-02 15:44:46 +0000

    add missing images to push_all.sh

--- push_all.sh
+++ push_all.sh
@@ -1,19 +1,12 @@
 ROCM_VERSION=5.3
 sudo docker push rocm/dev-ubuntu-20.04:$ROCM_VERSION
-sudo docker push rocm/dev-ubuntu-22.04:$ROCM_VERSION
-sudo docker push rocm/dev-centos-7:$ROCM_VERSION
-sudo docker push rocm/rocm-terminal:$ROCM_VERSION
-
-#ubuntu20.04 complete
 sudo docker push rocm/dev-ubuntu-20.04:$ROCM_VERSION-complete
-
-#ubuntu22.04 complete
-sudo docker push rocm/dev-ubuntu-22.04:$ROCM_VERSION-complete
-
-#centos complete
-#sudo docker push rocm/dev-centos-7:$ROCM_VERSION-complete
-
-sudo docker push rocm/dev-centos-7:latest
 sudo docker push rocm/dev-ubuntu-20.04:latest
+sudo docker push rocm/dev-ubuntu-22.04:$ROCM_VERSION
+sudo docker push rocm/dev-ubuntu-22.04:$ROCM_VERSION-complete
 sudo docker push rocm/dev-ubuntu-22.04:latest
+sudo docker push rocm/dev-centos-7:$ROCM_VERSION
+sudo docker push rocm/dev-centos-7:$ROCM_VERSION-complete
+sudo docker push rocm/dev-centos-7:latest
+sudo docker push rocm/rocm-terminal:$ROCM_VERSION
 sudo docker push rocm/rocm-terminal:latest
