c93695a72ee753377a5d9d9f7f62b36ef315de1c
Jeff Daily <jeff.daily@amd.com>
2023-06-02 15:41:36 +0000

    add Dockerfile-centos-7-complete

--- build_all.sh
+++ build_all.sh
@@ -14,6 +14,9 @@ cd ../dev
 sudo docker build . -f Dockerfile-centos-7 -t rocm/dev-centos-7:$ROCM_VERSION --build-arg=ROCM_VERSION=$ROCM_VERSION  --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
 sudo docker tag rocm/dev-centos-7:$ROCM_VERSION rocm/dev-centos-7:latest
 
+#centos-7 complete
+sudo docker build . -f Dockerfile-centos-7-complete -t rocm/dev-centos-7:$ROCM_VERSION-complete --build-arg=ROCM_VERSION=$ROCM_VERSION  --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
+
 #manylinux2014_x86_64
 sudo docker build . -f Dockerfile-manylinux2014_x86_64 -t rocm/dev-manylinux2014_x86_64:$ROCM_VERSION --build-arg=ROCM_VERSION=$ROCM_VERSION  --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
 sudo docker tag rocm/dev-manylinux2014_x86_64:$ROCM_VERSION rocm/dev-manylinux2014_x86_64:latest
@@ -22,6 +25,9 @@ sudo docker tag rocm/dev-manylinux2014_x86_64:$ROCM_VERSION rocm/dev-manylinux20
 sudo docker build . -f Dockerfile-ubuntu-20.04 -t rocm/dev-ubuntu-20.04:$ROCM_VERSION --build-arg=ROCM_VERSION=$ROCM_VERSION  --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
 sudo docker tag rocm/dev-ubuntu-20.04:$ROCM_VERSION rocm/dev-ubuntu-20.04:latest
 
+#ubuntu20.04 complete
+sudo docker build . -f Dockerfile-ubuntu-20.04-complete -t rocm/dev-ubuntu-20.04:$ROCM_VERSION-complete --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
+
 #ubuntu22.04
 sudo docker build . -f Dockerfile-ubuntu-22.04 -t rocm/dev-ubuntu-22.04:$ROCM_VERSION  --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION --build-arg=APT_PREF="Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600"
 sudo docker tag rocm/dev-ubuntu-22.04:$ROCM_VERSION rocm/dev-ubuntu-22.04:latest
@@ -29,5 +35,3 @@ sudo docker tag rocm/dev-ubuntu-22.04:$ROCM_VERSION rocm/dev-ubuntu-22.04:latest
 #ubuntu22.04 complete
 sudo docker build . -f Dockerfile-ubuntu-22.04-complete -t rocm/dev-ubuntu-22.04:$ROCM_VERSION-complete --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION --build-arg=APT_PREF="Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600"
 
-#ubuntu20.04 complete
-sudo docker build . -f Dockerfile-ubuntu-20.04-complete -t rocm/dev-ubuntu-20.04:$ROCM_VERSION-complete --build-arg=ROCM_VERSION=$ROCM_VERSION --build-arg=AMDGPU_VERSION=$AMDGPU_VERSION
